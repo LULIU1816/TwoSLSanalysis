0b4ebebf8b50528eae7e8395b5b3d842f44b9c7c
LULIU1816 <53689329+LULIU1816@users.noreply.github.com>
2019-08-29 18:37:09 +0800

    Delete selectmethysample.R

--- R/selectmethysample.R
+++
@@ -1,7 +0,0 @@
-selectmethysample <- function(methylationLUAD){
-  index <- substr(colnames(methylationLUAD), 14, 16)
-  methylationLUAD1 <- methylationLUAD[, index == "01A"]
-  methylationLUAD2 <- methylationLUAD[, index == "11A"]
-  methylationLUAD <- cbind(methylationLUAD1, methylationLUAD2)
-  return(methylationLUAD)
-}
